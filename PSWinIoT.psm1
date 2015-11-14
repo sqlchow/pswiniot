84a9e742791a6e34b2dfd1618915db1db1caf075
sqlchow <sqlchow@outlook.com>
2015-11-15 03:13:41 +0530

    sqlchow-pull-001
    I was only able to add few parameters out of the available set on the
    original Invoke-WebRequest.
    
    httpclienthandler class currently does not support:
    - addition of certificates. I mean, no real way to add certificates.
    - adding proxy, system.net.webproxy not available.
    
    For processing JSON data, I thought we could use Doug Finke's
    ConvertFrom-Json function which was written just before v3 release.
    However, it did not seem like it was working.
    https://powershelljson.codeplex.com/

--- PSWinIoT.psm1
+++ PSWinIoT.psm1
@@ -12,6 +12,10 @@ function Invoke-WinIoTWebRequest
 .EXAMPLE
    $r = Invoke-WinIoTWebRequest -URI https://github.com/stefanstranger/pswiniot
    $r.AllElements | where {$_.innerhtml -like "*=*"} | Sort { $_.InnerHtml.Length } | Select InnerText -First 5
+.EXAMPLE
+   $testUri = 'https://en.wikipedia.org/w/api.php?action=query&prop=info&format=json'+
+                                '&inprop=watched&titles=Main%20page'
+   Invoke-WinIoTWebRequest -URI $testUri
 .INPUTS
    Inputs to this cmdlet (if any)
 .OUTPUTS
@@ -43,19 +47,290 @@ function Invoke-WinIoTWebRequest
                    ParameterSetName='Default')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
-        $Uri
+        [uri]
+        $Uri,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1,
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        $Body,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=2,
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [switch]
+        $Certificate,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=3,
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $CertificateThumbprint,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=4,
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $ContentType = 'application/x-www-form-urlencoded',
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=5,
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=6, 
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [hashtable]
+        $Headers,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=7, 
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('DELETE', 'GET', 'HEAD', 'POST', 'PUT')]
+        [string]
+        $Method = 'GET',
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=8, 
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [uri]
+        $Proxy,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=9, 
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [pscredential]
+        $ProxyCredentials,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=10, 
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [switch]
+        $ProxyUseDefaultCredentials,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=10, 
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [switch]
+        $UseDefaultCredentials,
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=11, 
+                   ParameterSetName='Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $UserAgent
     )
 
     Begin
     {
+        $PSBoundParameters.GetEnumerator() | % { 
+            Write-Verbose "Parameter: $_" 
+        }
+
+        [void][System.Reflection.Assembly]::LoadWithPartialName('System.Net.Http')
+
+        $clientHandler = New-Object System.Net.Http.HttpClientHandler
+        $request       = New-Object System.Net.Http.HttpRequestMessage
+        $response      = New-Object System.Net.Http.HttpResponseMessage
+        $completionOption = [System.Net.Http.HttpCompletionOption]::ResponseContentRead
+
+        if($PSBoundParameters.ContainsKey('Uri')){
+            $request.RequestUri = $Uri
+        }
+
+        if($PSBoundParameters.ContainsKey('Body')){
+            $request.Content = `
+                New-Object System.Net.Http.StringContent($Body, [Encoding]::UTF8, $ContentType)
+        }
+
+        if($PSBoundParameters.ContainsKey('Certificate')){
+            $clientHandler.ClientCertificateOptions = `
+                        [System.Net.Http.ClientCertificateOption]::Automatic
+            <#
+                Currently, did not find a way of adding certificate to httpclient.
+
+                Found one way, it was too complicated.
+                http://piotrwalat.net/client-certificate-authentication-in-asp-net-web-api-and-windows-store-apps/
+                May not be required after all: 
+                https://pfelix.wordpress.com/2012/12/16/using-httpclient-with-ssltls/
+
+                changing from System.Security.Cryptography.X509Certificates.X509Certificate2 to switch
+
+                try{
+                    $clientCert = [System.Security.Cryptography.X509Certificates.X509Certificate `
+                                ]::CreateFromCertFile($Certificate)
+                }catch [ArgumentException]{
+                    Write-Error -Message "The $Certificate parameter is null."
+                }catch {
+                    Write-Error -Message "Could not load $Certificate ."
+                }
+            #>
+        }
+        
+        if($PSBoundParameters.ContainsKey('CertificateThumbprint')){
+            # Need to figure this out.
+            # Get-CertificateFromThumbPrint -Thumbprint $CertificateThumbprint
+        }
+
+        if($PSBoundParameters.ContainsKey('ContentType')){
+            $mediatype = New-Object `
+                                -TypeName System.Net.Http.Headers.MediaTypeWithQualityHeaderValue `
+                                -ArgumentList $ContentType
+        }
+
+        if($PSBoundParameters.ContainsKey('Credential')){
+            $useAltCredentails  = $true
+            $networkCredentails = $Credential.GetNetworkCredential()
+            $encodedByteArray   = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes( `
+                                "$($networkCredentails.UserName):$($networkCredentails.Password)"))
+
+            $clientHandler.PreAuthenticate = $true
+        }
+
+        if($PSBoundParameters.ContainsKey('Method')){
+            switch ($Method)
+            {
+                {$_ -in 'GET','HEAD'} {
+                    $request.Method = [System.Net.Http.HttpMethod]::Get
+                 }
+                'POST'   {
+                    $request.Method = [System.Net.Http.HttpMethod]::Post
+                 }
+                'PUT'    {
+                    $request.Method = [System.Net.Http.HttpMethod]::Put
+                 }
+                'DELETE' {
+                    $request.Method = [System.Net.Http.HttpMethod]::Delete
+                 }
+                Default {
+                    $request.Method = [System.Net.Http.HttpMethod]::Get
+                 }
+            }
+        }
+
+        if($PSBoundParameters.ContainsKey('Proxy')){
+            #Need to figure this out as System.Net.WebProxy is not available
+            #$clienthandler.Proxy = [System.Net.WebProxy]($Proxy)
+            #$clientHandler.UseProxy = $true
+        }
+        if($PSBoundParameters.ContainsKey('ProxyCredentials')){
+            #Need to figure this out as System.Net.WebProxy is not available
+        }
+
+        if($PSBoundParameters.ContainsKey('ProxyUseDefaultCredentials')){
+            #Need to figure this out as System.Net.WebProxy is not available
+        }
+
+        if($PSBoundParameters.ContainsKey('UseDefaultCredentials')){
+            $clientHandler.UseDefaultCredentials = $true
+        }
+
+        if($PSBoundParameters.ContainsKey('UserAgent')){
+            $addUserAgent = $true
+        }
+
     }
     Process
     {
-            #Create httpclient object
-            $httpclient = new-object system.net.http.httpclient
-            $httpclient.GetStringAsync($Uri).Result
+            
+            
+            #
+            $httpclient = New-Object -TypeName System.Net.Http.HttpClient -ArgumentList $clientHandler
+
+            if($PSBoundParameters.ContainsKey('Headers')){
+                foreach ($item in $Headers.Keys)
+                {
+                  if(-not $httpclient.DefaultRequestHeaders.Contains($item)){
+                    $httpclient.DefaultRequestHeaders.Add($item, $Headers[$item])
+                  }
+                }
+            }
+
+            if($mediatype){
+                $httpclient.DefaultRequestHeaders.Accept.Add($mediatype)
+            }
+                        
+            if($addUserAgent){
+                $httpclient.DefaultRequestHeaders.UserAgent.Add($UserAgent)
+            }else{
+                $httpclient.DefaultRequestHeaders.Add("User-Agent", "winIoT-webrequest")
+            }
+
+            if($useAltCredentails){
+                $httpclient.DefaultRequestHeaders.Authorization = `
+                    New-Object System.Net.Http.Headers.AuthenticationHeaderValue("Basic", $encodedByteArray)
+                
+            }
+
+            
+            #$httpclient.GetStringAsync($Uri).Result
+            $response = $httpclient.SendAsync($request, $completionOption)
+            
+            while(-not $response.IsCompleted){
+                Write-Verbose "Awaiting reponse: $($response.AsyncState)"
+                
+                $response.Wait()
+                Write-Verbose "Sleeping for 200ms"
+                Start-Sleep -Milliseconds 200
+            }
+
+            $response.Result.Content.ReadAsStringAsync().Result
     }
     End
     {
+        $response.Dispose()
+        $request.Dispose()
+        $clientHandler.Dispose()
+        $httpclient.Dispose()
     }
 }
